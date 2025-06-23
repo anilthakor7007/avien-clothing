e448147191416365361d87b9a297069c14a84beb
Anil <anilthakor7007@gmail.com>
2025-06-23 23:57:50 +0530

    install pnpm and run build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@remix-run/router']
+  },
+  build: {
+    rollupOptions: {
+      external: ['@remix-run/router'],
+      // Limit parallel file operations to reduce EMFILE errors
+      maxParallelFileOps: 10
+    }
+  }
 })
